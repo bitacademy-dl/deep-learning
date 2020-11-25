38bbb8098cb9cd80e62b94a26a84ec347e929720
bitacademy-dl <bitacademy.dl@gmail.com>
2020-11-25 10:20:54 +0900

    mnist 신호전달 #1

--- 02.neural-network/05.mnist-neural-network/ex01.py
+++ 02.neural-network/05.mnist-neural-network/ex01.py
@@ -1,11 +1,8 @@
 # MNIST(Modified National Institude of Standard and Technology)
 # MNIST 손글씨 숫자 분류 신경망(Neural Network for MNIST Handwritten Digit Classification): 데이터 살펴보기
-
 import os
 import sys
-import numpy as np
 from PIL import Image
-from matplotlib import pyplot as plt
 from pathlib import Path
 try:
     sys.path.append(os.path.join(Path(os.getcwd()).parent, 'lib'))
@@ -14,7 +11,7 @@ except ImportError:
     print('Library Module Can Not Found')
 
 
-(train_x, train_t), (test_x, test_t) = load_mnist(normalize=False, flatten=True, one_hot_label=False)
+(train_x, train_t), (test_x, test_t) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
 print(train_x.shape)   # 60,000 x 784 (matrix)
 print(train_t.shape)   # 60,000 (Vector)
 
