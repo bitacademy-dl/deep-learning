c1f97b8e450c60cd374a30ff46d96e3f9b0aaad3
bitacademy-dl <bitacademy.dl@gmail.com>
2020-11-26 13:46:59 +0900

    numerical_diff 수정 #1

--- 02.neural-network/06.neural-network-training/ex04-1.py
+++ 02.neural-network/06.neural-network-training/ex04-1.py
@@ -12,15 +12,18 @@ except ImportError:
 
 def loss(w, x, t):
     a = np.dot(x, w)
-    y = softmax(a)
+    y = softmax(a)                   # softmax(x @ w)
     e = cross_entropy_error(y, t)
 
     return e
 
 
-_x = np.array([0.6, 0.9])   # 입력(x)          2 vector
-_t = np.array([0., 0., 1.]) # label(one-hot)  3 vector
-_w = np.random.randn(2, 3)  # weight          2 x 3 matrix
+_x = np.array([0.6, 0.9])       # 입력(x)          2 vector
+_t = np.array([0., 0., 1.])     # label(one-hot)  3 vector
+_w = np.array([
+    [0.02, 0.224, 0.135],
+    [0.01, 0.052, 0.345]
+])                              # weight          2 x 3 matrix
 
 g = numerical_gradient1(loss, _w, _x, _t)
 
