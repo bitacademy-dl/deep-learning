4c9f215611c261a11e3d863f5fafa453a6f9088d
bitacademy-dl <bitacademy.dl@gmail.com>
2020-11-27 14:35:34 +0900

    ex05 two layer network training 완성

--- 02.neural-network/06.neural-network-training/ex07.py
+++ 02.neural-network/06.neural-network-training/ex07.py
@@ -26,11 +26,12 @@ test_accuracy = network.accuracy(test_x, test_t)
 
 print(train_accuracy, test_accuracy)
 
-
+#
 # train_accuracy와 test_accuracy가 일치하는 것은 Overfitting이 발생하지 않았다는 것이다.
 # 학습 중에 1epoch 별로 train/test accuracy를 각각 기록하여 추이를 비교하여야 한다.
 # 두 accuracy가 마지막까지 차이가 없는 것이 가장 바람직 하며
 # 만약 차이가 나면 차이가 나는 그 시점을 찾아서 학습을 중지 하여야 한다 - early stopping
+#
 
 
 
