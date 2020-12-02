1a4580eeaf0d507836cd91762f2cb2895131a6c9
bitacademy-dl <bitacademy.dl@gmail.com>
2020-12-02 15:21:41 +0900

    Tensorflow-keras 연습#2

--- 03.tensorflow-keras/05.mnist/ex01.py
+++ 03.tensorflow-keras/05.mnist/ex01.py
@@ -1,10 +1,10 @@
 # MNIST handwritten digit classification model
 import os
 import sys
-
+import numpy as np
+from matplotlib import pyplot as plt
 from tensorflow.python.keras import Sequential
 from tensorflow.python.keras.layers import Dense
-
 try:
     sys.path.append(os.path.join(os.getcwd(), 'lib'))
     from mnist import load_mnist
@@ -23,4 +23,20 @@ model.add(Dense(train_t.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
 # 4. model fitting
-model.fit(train_x, train_t, epochs=30, batch_size=100, verbose=1)
+history = model.fit(train_x, train_t, validation_data=(test_x, test_t), epochs=30, batch_size=100, verbose=1)
+
+# 5. train loss
+train_loss = history.history['loss']
+test_loss = history.history['val_loss']     # validation loss(test loss)
+
+# 6. graph
+xlen = np.arange(len(train_loss))
+
+plt.plot(xlen, train_loss, marker='.', c='blue', label='Train Loss')
+plt.plot(xlen, test_loss, marker='.', c='red', label='Test Loss')
+
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend(loc='best')
+
+plt.show()
